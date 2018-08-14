bc5d4f12282b9af50c3f2b64f314f76371496a9a
Kamil S Jaroň <376090@mail.muni.cz>
2018-08-14 14:25:08 +0200

    interface to blurplot.R, readme file

--- README.md
+++ README.md
@@ -2,9 +2,46 @@
 
 This tool extract heterozygous kmer pairs from jellyfish dump files and does a gymnastics with it. We are able to disentangle genome stricture by comparison of the sum of kmer pair coverages and their relative coverage. Such approach allows to analyze also obscure genomes with duplications, various ploidy levels and stuff. it's work in progress but it's going to be great.
 
-Content :
+## Content
 
 - [hetkmers.py](hetkmers.py): python script to compute heterozygous kmer pairs
 - [blurplot.R](blurplot.R): R script that creates so-called blur plot from `coverages_2.tsv` file - a two column file with kmer sorted kmer coverages.
 
 - [playground.py](playground.py): snippets of python code for testing that should be maintained.
+
+## Usage
+
+Right now the workflow is semi-automatic. It requires some decisions. The first step is to run [jellyfish](https://github.com/gmarcais/Jellyfish) and [GenomeScope](https://github.com/schatzlab/genomescope).
+
+Give jellyfish all the files with trimmed reads to calculate kmer frequencies and then generate histogram of kmers:
+
+```
+jellyfish count -C -m 21 -s 1000000000 -t 8 *.fastq -o kmer_counts.jf
+jellyfish histo kmer_counts.jf > kmer_k21.hist
+```
+
+Now feed the histogram to GenomeScope (either run the [script](https://github.com/schatzlab/genomescope/blob/master/genomescope.R) or use their [web server](http://qb.cshl.edu/genomescope/))
+
+```
+Rscript genomescope.R kmer_k21.hist <k-mer_length> <read_length> <output_dir> [kmer_max] [verbose]
+```
+
+You just made estimates of genome size, heterozygosity and repetitive fraction of the genome. Look at the fitted model and figure out what is the smallest peak after the error tail. Now you need to decide the low end cutoff that will discard all the kmers that have that small coverage and look like errors (cca 1/2 of the haplod kmer coverage), also check the right side of the graph to find reasonable upped threshold (cca 8x of the haploid kmer coverage). Dump kmers in the coverage range you have defined using jellyfish and pipe it directly to the python script that will compute set of heterozygous kmers.
+
+```
+jellyfish dump -c -L 40 -U 600 kmer_counts.jf | hetkmers.py -o kmer_pairs
+```
+
+Now finally generate blur plot using coverages of the kmer pairs. You need to supply the haploid kmer coverage (reported by GenomeScope). If GenomeScope correctly identified the peak of haplod kmers, the expected positions of haplotype structures will overlap with high density blurs on the blurplot. If the overlap is not great you might consider to adjust both GenomeScope model and redo the plod with the better estimate of the haploid coverage.
+
+```
+blurplot.R <haploid_coverage> kmer_pairs
+```
+
+The blur plot uses colouration on squared scale, the legend indicate approximate kmer pairs per tile densities. Note that single polymorphism generates multiple heterozygous kmers, these numbers do not directly correspond to variants, but should be fairly correlated.
+
+## TODO
+
+- nicer scale on the y axis (display ticks on 2n, 3n, 4n, ...)
+- quantify relative proportions of blurs
+- generate summary report
--- blurplot.R
+++ blurplot.R
@@ -1,78 +1,99 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+# args[1] - input tsv file
+# args[2] - output [default: blurplot.png]
+
+if(length(args) < 1) {
+      stop("No arguments supplied.\nUsage: blurplot.R haplod_cov [input.tsv] [output.png]", call.=FALSE)
+} else {
+      n <- as.numeric( args[1] )
+}
+
+infile <- ifelse(length(args) < 2, 'coverages_2.tsv', args[2])
+outfile <- ifelse(length(args) < 3, 'blurplot.png', args[3])
+
+library(methods)
 library(MASS) # smoothing
 library(RColorBrewer)
-library(hexbin) # honeycomb plot
+# library(hexbin) # honeycomb plot
 
 pal <- brewer.pal(11,'Spectral')
 rf <- colorRampPalette(rev(pal[3:11]))
 r <- rf(32)
 
-cov <- read.table('coverages_2.tsv')
+cov <- read.table(infile)
 
+# calcualte relative coverage of the minor allele
 minor_variant_rel_cov <- cov$V1 / (cov$V1 + cov$V2)
+# total covarate of the kmer pair
 total_pair_cov <- cov$V1 + cov$V2
 
+# quantile filtering (remove top 1%, it's not really informative)
 high_cov_filt <- quantile(total_pair_cov, 0.99) > total_pair_cov
 minor_variant_rel_cov <- minor_variant_rel_cov[high_cov_filt]
 total_pair_cov <- total_pair_cov[high_cov_filt]
 
+# calculate historgrams
 h1 <- hist(minor_variant_rel_cov, breaks = 100, plot = F)
 h2 <- hist(total_pair_cov, breaks = 100, plot = F)
 
 top <- max(h1$counts, h2$counts)
-k <- kde2d(minor_variant_rel_cov, total_pair_cov, n=30)
+k <- kde2d(minor_variant_rel_cov, total_pair_cov, n=10)
 k$z <- sqrt(k$z)
 
-png('~/Desktop/Avag1_kmer_pair_coverage.png')
-# margins 'c(bottom, left, top, right)'
-par(mar=c(4.8,4.8,1,1))
-layout(matrix(c(2,4,1,3), 2, 2, byrow=T), c(3,1), c(1,3))
-#plot the image
-# 2D HISTOGRAM
-image(k, col = r,
-      xlab = 'Normalized minor kmer coverage',
-      ylab = 'Total coverage of the kmer pair', cex.lab = 1.4
-)
-# TODO image ticks by the coverage counts 2n, 3n, 4n, 5n, 6n, 7n, 8n...
+png(outfile)
+      # margins 'c(bottom, left, top, right)'
+      par(mar=c(4.8,4.8,1,1))
+      layout(matrix(c(2,4,1,3), 2, 2, byrow=T), c(3,1), c(1,3))
 
-n <- 92
-# for(i in 2:6){
-#       lines(c(0, 0.6), rep(i * n, 2), lwd = 1.4)
-#       text(0.1, i * n, paste0(i,'x'), pos = 3)
-# }
+      # 2D HISTOGRAM
+      # TODO image ticks by the coverage counts 2n, 3n, 4n, 5n, 6n, 7n, 8n...
+      image(k, col = r,
+            xlab = 'Normalized minor kmer coverage',
+            ylab = 'Total coverage of the kmer pair', cex.lab = 1.4
+      )
 
-# EXPECTED COMPOSITIONS
-text(1/2 - 0.01, 2 * n, 'AB', offset = 0, cex = 1.4)
-text(1/3, 3 * n, 'AAB', offset = 0, cex = 1.4)
-text(1/4, 4 * n, 'AAAB', offset = 0, cex = 1.4)
-text(1/2 - 0.022, 4 * n, 'AABB', offset = 0, cex = 1.4)
-text(2/5, 5 * n, 'AAABB', offset = 0, cex = 1.4)
-text(1/5, 5 * n, 'AAAAB', offset = 0, cex = 1.4)
-text(3/6 - 0.035, 6 * n, 'AAABBB', offset = 0, cex = 1.4)
-text(2/6, 6 * n, 'AAAABB', offset = 0, cex = 1.4)
-text(1/6, 6 * n, 'AAAAAB', offset = 0, cex = 1.4)
+      # TEST plot lines at expected coverages
+      # for(i in 2:6){
+      #       lines(c(0, 0.6), rep(i * n, 2), lwd = 1.4)
+      #       text(0.1, i * n, paste0(i,'x'), pos = 3)
+      # }
 
-# minor_variant_rel_cov HISTOGRAM
-par(mar=c(0,3.8,1,0))
-barplot(h1$counts, axes=F, ylim=c(0, top), space=0, col = pal[2])
-# total pair coverage HISTOGRAM
-par(mar=c(3.8,0,0.5,1))
-barplot(h2$counts, axes=F, xlim=c(0, top), space=0, col = pal[2], horiz=T)
+      # EXPECTED COMPOSITIONS - bettern than lines
+      text(1/2 - 0.01, 2 * n, 'AB', offset = 0, cex = 1.4)
+      text(1/3, 3 * n, 'AAB', offset = 0, cex = 1.4)
+      text(1/4, 4 * n, 'AAAB', offset = 0, cex = 1.4)
+      text(1/2 - 0.022, 4 * n, 'AABB', offset = 0, cex = 1.4)
+      text(2/5, 5 * n, 'AAABB', offset = 0, cex = 1.4)
+      text(1/5, 5 * n, 'AAAAB', offset = 0, cex = 1.4)
+      text(3/6 - 0.035, 6 * n, 'AAABBB', offset = 0, cex = 1.4)
+      text(2/6, 6 * n, 'AAAABB', offset = 0, cex = 1.4)
+      text(1/6, 6 * n, 'AAAAAB', offset = 0, cex = 1.4)
 
-# LEGEND
-par(mar=c(0,0,2,1))
-plot.new()
-title('Coverage (^2 scale)')
-for(i in 1:32){
-      rect(0,(i - 0.01) / 33, 0.5, (i + 0.99) / 33, col = r[i])
-}
-kmer_max <- (length(total_pair_cov) * max((k$z)^2)) / sum((k$z)^2)
-for(i in 0:6){
-      text(0.75, i / 6, round((sqrt(kmer_max) * i)^2 / 6000) * 1000, offset = 0)
-}
+      # minor_variant_rel_cov HISTOGRAM - top
+      par(mar=c(0,3.8,1,0))
+      barplot(h1$counts, axes=F, ylim=c(0, top), space=0, col = pal[2])
+
+      # total pair coverage HISTOGRAM - right
+      par(mar=c(3.8,0,0.5,1))
+      barplot(h2$counts, axes=F, xlim=c(0, top), space=0, col = pal[2], horiz=T)
+
+      # LEGEND (topright corener)
+      par(mar=c(0,0,2,1))
+      plot.new()
+      title('coverage (^2 scale)')
+      for(i in 1:32){
+            rect(0,(i - 0.01) / 33, 0.5, (i + 0.99) / 33, col = r[i])
+      }
+      kmer_max <- (length(total_pair_cov) * max((k$z)^2)) / sum((k$z)^2)
+      for(i in 0:6){
+            text(0.75, i / 6, round((sqrt(kmer_max) * i)^2 / 6000) * 1000, offset = 0)
+      }
 
 dev.off()
 
-# alternative plotting
+## alternative plotting
 # h <- hexbin(df)
 # # h@count <- sqrt(h@count)
 # plot(h, colramp=rf)
