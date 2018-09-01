b49d6bf939a821a07174eb13bb1285e00e154656
Timothy Rhyker Ranallo-Benavidez <tbenavi1@jhmi.edu>
2018-09-01 18:20:29 -0400

    Update hetkmers.py

--- hetkmers.py
+++ hetkmers.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from collections import defaultdict, Counter
-from itertools import combinations
-# import matplotlib.pyplot as plt
-import networkx as nx
-import pandas as pd
-import pickle
+from collections import defaultdict
+from multiprocessing import Pool
+import itertools
 import sys
 import argparse
 
@@ -13,9 +10,8 @@ import argparse
 # GLOBAL VARIABLES #
 ####################
 
-# every time you called kmer_to_int function you created a new dictionary
-# that's costly and you can do it just once
-letter_to_int = {'A':1, 'C':2, 'G':3, 'T':4}
+letter_to_int = {'A':0, 'C':1, 'G':2, 'T':3}
+convertString = "ACGT"
 
 ###################
 ###  DEFINE     ###
@@ -23,153 +19,151 @@ letter_to_int = {'A':1, 'C':2, 'G':3, 'T':4}
 ###################
 
 def kmer_to_int(kmer):
-  """This function turns a kmer string into an integer."""
+  """This function converts a kmer string into an integer."""
   k = len(kmer)
-  # souradnice = strtol(zkoumaneslovo, NULL, 4);
-  return sum([4**(k-i-1) * letter_to_int[letter] for i, letter in enumerate(kmer)])
+  return sum((4**(k-i-1) * letter_to_int[letter] for i, letter in enumerate(kmer)))
 
-def get_1away_pairs(local_index_to_kmer, k):
-  """local_index_to_kmer is a dictionary where the value is a kmer portion, and the key is the index of the original kmer in which the kmer portion is found. get_1away_pairs returns a list of pairs of indices where each pair of indices correspond to a pair of kmers different in exactly one base."""
+def int_to_kmer(n):
+  """This function converts an integer into a kmer string. The variable k refers to the kmer length."""
+  def int_to_truncated_kmer(n):
+    if n < 4:
+      return convertString[n]
+    else:
+      return int_to_truncated_kmer(n//4) + convertString[n%4]
+  truncated_kmer = int_to_truncated_kmer(n)
+  prefix = 'A'*(k - len(truncated_kmer))
+  return prefix + truncated_kmer
 
-  #This is the base case for the recursion. Return every pair of indices where the kmers at those indices differ at exactly one base.
+def get_1away_pairs(kmer_ints, local_indices, i_L, i_R):
+  """kmer_ints is the list of kmer_ints. local_indices is a list of indices of the kmer portions currently under consideration. i_L and i_R are the start and end locations of the kmer portions currently under consideration (0-indexed and inclusive). get_1away_pairs returns a list of pairs of indices where each pair of indices corresponds to a pair of kmers different in exactly one base."""
+
+  #This is the base case for the recursion. Return every pair of indices where the kmer portions (between locations i_L and i_R inclusive) at those indices differ at exactly one base.
+  k = i_R - i_L + 1
   if k == 1:
-    return [(i,j) for (i,j) in combinations(local_index_to_kmer, 2) if local_index_to_kmer[i][0] != local_index_to_kmer[j][0]]
+    return [(i,j) for (i,j) in itertools.combinations(local_indices, 2) if int_to_kmer(kmer_ints[i])[i_L:i_R+1] != int_to_kmer(kmer_ints[j])[i_L:i_R+1]]
 
-  #Get the two halves of the kmer
+  #Get the locations for the two halves of the kmer portion
   k_L = k // 2
-  k_R = k-k_L
+  i_L_L = i_L
+  i_L_R = i_L + k_L - 1
+  i_R_L = i_L + k_L
+  i_R_R = i_R
 
-  #initialize dictionaries in which the key is the hash of half of the kmer, and the value is a list of indices of the kmers with that same hash
-  kmer_L_hashes = defaultdict(list)
-  kmer_R_hashes = defaultdict(list)
+  #initialize dictionaries in which the key is half of the kmer portion (kmer_L or kmer_R respectively), and the value is a list of indices ("index_family") corresponding to the kmer portions with that same half
+  kmer_L_to_index_family = defaultdict(list)
+  kmer_R_to_index_family = defaultdict(list)
 
   #initialize pairs, which will be returned by get_1away_pairs
   pairs = []
 
-  #for each kmer, calculate its left hash and right hash, then add its index to the corresponding entries of the dictionary
-  for i, kmer in local_index_to_kmer.items():
-    kmer_L = kmer[:k_L]
-    kmer_R = kmer[k_L:]
-    kmer_L_hashes[kmer_to_int(kmer_L)] += [i]
-    kmer_R_hashes[kmer_to_int(kmer_R)] += [i]
+  #for each index, calculate the corresponding left half and right half, then add its index to the corresponding entries of the dictionary
+  for i in local_indices:
+    kmer_L = int_to_kmer(kmer_ints[i])[i_L_L:i_L_R+1]
+    kmer_R = int_to_kmer(kmer_ints[i])[i_R_L:i_R_R+1]
+    kmer_L_to_index_family[kmer_to_int(kmer_L)] += [i]
+    kmer_R_to_index_family[kmer_to_int(kmer_R)] += [i]
 
-  #for each left hash in which there are multiple kmers with that left hash, find the list of pairs in which the right half differs by 1. (aka, if left half matches, recurse on right half).
-  for kmer_L_hash_indices in kmer_L_hashes.values(): #same in first half
-    if len(kmer_L_hash_indices) > 1:
-      pairs += get_1away_pairs({kmer_L_hash_index:local_index_to_kmer[kmer_L_hash_index][k_L:] for kmer_L_hash_index in kmer_L_hash_indices}, k_R) #differ by 1 in right half
+  #discard kmer portion halves, keep indices
+  kmer_L_index_families = kmer_L_to_index_family.values()
+  del kmer_L_to_index_family
+  kmer_R_index_families = kmer_R_to_index_family.values()
+  del kmer_R_to_index_family
 
-  #for each right hash in which there are multiple kmers with that right hash, find the list of pairs in which the left half differs by 1. (aka, if right half matches, recurse on left half).
-  for kmer_R_hash_indices in kmer_R_hashes.values(): #same in right half
-    if len(kmer_R_hash_indices) > 1:
-      pairs += get_1away_pairs({kmer_R_hash_index:local_index_to_kmer[kmer_R_hash_index][:k_L] for kmer_R_hash_index in kmer_R_hash_indices}, k_L) #differ by 1 in left half
+  #for each left half in which there are multiple kmer portions with that left half, find the list of pairs in which the right half differs by 1. (aka, if left half matches, recurse on right half).
+  for kmer_L_index_family in kmer_L_index_families: #same in left half
+    if len(kmer_L_index_family) > 1:
+      pairs += get_1away_pairs(kmer_ints, kmer_L_index_family, i_R_L, i_R_R) #differ by 1 in right half
+
+  #for each right half in which there are multiple kmer portions with that same right half, find the list of pairs in which the left half differs by 1. (aka, if right half matches, recurse on left half).
+  for kmer_R_index_family in kmer_R_index_families: #same in right half
+    if len(kmer_R_index_family) > 1:
+      pairs += get_1away_pairs(kmer_ints, kmer_R_index_family, i_L_L, i_L_R) #differ by 1 in left half
   return(pairs)
 
 ##############
 ### SCRIPT ###
 ##############
+if __name__=='__main__':
+  parser = argparse.ArgumentParser(description='Calculate unique kmer pairs from a Jellyfish or KMC dump file.')
+  parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='Jellyfish input dump file (stdin).')
+  parser.add_argument('-o', help='The pattern used to name the output (kmerpairs).', default='kmerpairs')
+  parser.add_argument('-k', help='The length of the kmer.', default=21)
+  parser.add_argument('-t', help='Number of processes to use.', default = 5)                    
 
-parser = argparse.ArgumentParser(description='Calculate unique kmer pairs from a Jellyfish dump file.')
-parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
-                     default=sys.stdin, help='Jellyfish input dump file (stdin)')
-parser.add_argument('-o', help='the pattern used to name the output (kmerpairs)',
-                    default='kmerpairs')
+  args = parser.parse_args()
+  dumps_file = args.infile
+  output_pattern = args.o
+  k = int(args.k)
+  num_processes = int(args.t)
 
-args = parser.parse_args()
-dumps_file = args.infile
-output_pattern = args.o
-# the output string is
+  #Get the locations for the two halves of the kmer
+  k_L = k // 2
+  i_L_L = 0
+  i_L_R = k_L - 1
+  i_R_L = k_L
+  i_R_R = k-1
 
-#create a pandas dataframe of the dumps file
-df = pd.read_csv(dumps_file, sep = ' ', header = None)
+  #initiate list of kmer_ints and coverages
+  kmer_ints = []
+  coverages = []
 
-#get a list of kmers and coverages
-kmers = list(df[0])
-coverages = list(df[1])
+  #initialize dictionaries in which the key is half of the kmer (kmer_L or kmer_R respectively), and the value is a list of indices ("index_family") corresponding to the kmers with that same half
+  kmer_L_to_index_family = defaultdict(list)
+  kmer_R_to_index_family = defaultdict(list)
 
-#delete the dataframe
-del df
+  #read each line of the input file in order to load the kmers and coverages, and process the kmer halves
+  for i, line in enumerate(dumps_file):
+    kmer, coverage = line.split()
+    kmer_int = kmer_to_int(kmer)
+    coverage = int(coverage)
+    kmer_L = kmer[i_L_L:i_L_R+1]
+    kmer_R = kmer[i_R_L:i_R_R+1]
+    kmer_L_int = kmer_to_int(kmer_L)
+    kmer_R_int = kmer_to_int(kmer_R)
+    kmer_ints.append(kmer_int)
+    coverages.append(coverage)
+    kmer_L_to_index_family[kmer_L_int] += [i]
+    kmer_R_to_index_family[kmer_R_int] += [i]
 
-#get the value of k
-k = len(kmers[0])
+  print('Kmers and coverages loaded. Initial kmer halves processed.')
 
-#turn the kmers and coverages into a dictionary, where the key is the index of the original kmer
-test_kmers = {i:kmer for (i, kmer) in enumerate(kmers)}
-test_coverages = {i:coverage for (i, coverage) in enumerate(coverages)}
+  #discard kmer halves, keep indices
+  kmer_L_index_families = kmer_L_to_index_family.values()
+  del kmer_L_to_index_family
+  kmer_R_index_families = kmer_R_to_index_family.values()
+  del kmer_R_to_index_family
 
-#delete the kmers and coverages lists
-del kmers
-del coverages
+  print('Starting processes.')
 
-#get the one away pairs and save it to a pickle file
-one_away_pairs = get_1away_pairs(test_kmers, k)
-pickle.dump(one_away_pairs, open(output_pattern + '_one_away_pairs.p', 'wb'))
+  #create process pool to process the kmer half index families
+  with Pool(num_processes) as p:
+    pairs1 = p.starmap(get_1away_pairs, ((kmer_ints, kmer_L_index_family, i_R_L, i_R_R) for kmer_L_index_family in kmer_L_index_families if len(kmer_L_index_family)>1))
+    pairs2 = p.starmap(get_1away_pairs, ((kmer_ints, kmer_R_index_family, i_L_L, i_L_R) for kmer_R_index_family in kmer_R_index_families if len(kmer_R_index_family)>1))
+  one_away_pairs = list(itertools.chain(itertools.chain.from_iterable(pairs1), itertools.chain.from_iterable(pairs2)))
 
-#get the two away pairs and save it to a pickle file
-#two_away_pairs = get_2away_pairs(test_kmers, k)
-#pickle.dump(two_away_pairs, open('two_away_pairs.p', 'wb'))
+  print('Process pool completed.')
 
-#To determine the families, use the "connected_components" function of a networkx graph
-#add each of the pairs as edges to the graph, and save it to a pickle file
-G = nx.Graph()
-for one_away_pair in one_away_pairs:
-  G.add_edge(*one_away_pair)
-#for two_away_pair in two_away_pairs:
-#  G.add_edge(*two_away_pair)
-# pickle.dump(G, open(output_pattern + 'G.p', 'wb'))
+  #save one_away_pairs to a tsv file, keep track of which kmers only occur once
+  repeated = {}
+  with open(output_pattern + '_one_away_pairs.tsv', 'w') as record_file:
+    for (i1, i2) in one_away_pairs:
+      record_file.write(str(i1) + '\t' + str(i2) + '\n')
+      repeated[i1] = i1 in repeated
+      repeated[i2] = i2 in repeated
 
-#get the lengths of the families, save it to a pickle file, and then look at the Counter summary
-component_lengths = [len(x) for x in nx.connected_components(G)]
-# pickle.dump(component_lengths, open(output_pattern + '_component_lengths.p', 'wb'))
-Counter(component_lengths)
+  print('*_one_away_pairs.tsv file saved.')
 
-#get the families with 2-6 members
-families_2 = []
-families_3 = []
-families_4 = []
-families_5 = []
-families_6 = []
+  #save families_2 and coverages_2 tsv files, which only include one_away_pairs that don't overlap any others
+  with open(output_pattern + '_families_2.tsv', 'w') as record_file1, open(output_pattern + '_coverages_2.tsv', 'w') as record_file2:
+    for (i1, i2) in one_away_pairs:
+      if not repeated[i1] and not repeated[i2]:
+        cov1 = coverages[i1]
+        cov2 = coverages[i2]
+        if cov1 < cov2:
+          record_file1.write(str(i1) + '\t' + str(i2) + '\n')
+          record_file2.write(str(cov1) + '\t' + str(cov2) + '\n')
+        else:
+          record_file1.write(str(i2) + '\t' + str(i1) + '\n')
+          record_file2.write(str(cov2) + '\t' + str(cov1) + '\n')
 
-for component in nx.connected_components(G):
-  if len(component) == 2:
-    families_2.append(list(component))
-  elif len(component) == 3:
-    families_3.append(list(component))
-  elif len(component) == 4:
-    families_4.append(list(component))
-  elif len(component) == 5:
-    families_5.append(list(component))
-  elif len(component) == 6:
-    families_6.append(list(component))
-
-#save families_2 to a pickle file
-# pickle.dump(families_2, open(output_pattern + '_families_2.p', 'wb'))
-# perhaps faster way how to calculate coverages_2, probably more pythonic
-# coverages_2 = [test_coverages[cov_i1] + test_coverages[cov_i2] for cov_i1, cov_i2 in families_2]
-coverages_2 = [sum([test_coverages[i] for i in family]) for family in families_2]
-# pickle.dump(coverages_2, open(output_pattern + '_coverages_2.p', 'wb'))
-
-with open(output_pattern + '_coverages_2.tsv', 'w') as record_file:
-  for (cov_i1, cov_i2) in families_2:
-    cov1 = test_coverages[cov_i1]
-    cov2 = test_coverages[cov_i2]
-    if cov1 < cov2:
-      record_file.write(str(cov1) + '\t' + str(cov2) + '\n')
-    else:
-      record_file.write(str(cov2) + '\t' + str(cov1) + '\n')
-
-# save families_3 to a pickle file
-# pickle.dump(families_3, open(output_pattern + '_families_3.p', 'wb'))
-# coverages_3 = [sum([test_coverages[i] for i in family]) for family in families_3]
-#
-# # save families_4 to a pickle file
-# pickle.dump(families_4, open(output_pattern + '_families_4.p', 'wb'))
-# coverages_4 = [sum([test_coverages[i] for i in family]) for family in families_4]
-#
-# # save families_5 to a pickle file
-# pickle.dump(families_5, open(output_pattern + '_families_5.p', 'wb'))
-# coverages_5 = [sum([test_coverages[i] for i in family]) for family in families_5]
-#
-# # save families_6 to a pickle file
-# pickle.dump(families_6, open(output_pattern + '_families_6.p', 'wb'))
-# coverages_6 = [sum([test_coverages[i] for i in family]) for family in families_6]
-#
+  print('*_families_2.tsv and *_coverages_2.tsv files saved.')
