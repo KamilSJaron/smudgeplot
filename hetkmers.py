3eef45497bd1f0cf0113564f7e8084ff05a7cc7e
Kamil S Jaroň <376090@mail.muni.cz>
2019-01-21 15:34:07 +0100

    refactoring Rhykers code; fixing the order of reported kmers; related to #24

--- hetkmers.py
+++ hetkmers.py
@@ -66,17 +66,56 @@ def worker(q, results, k):
   q.close()
   results.close()
 
-##############
-### SCRIPT ###
-##############
-if __name__=='__main__':
-  parser = argparse.ArgumentParser(description='Calculate unique kmer pairs from a Jellyfish or KMC dump file.')
-  parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='Jellyfish or KMC input dump file (stdin).')
-  parser.add_argument('-o', help='The pattern used to name the output (kmerpairs).', default='kmerpairs')
-  parser.add_argument('-k', help='The length of the kmer.', default=21)
-  parser.add_argument('-t', help='Number of processes to use.', default = 4)
+#########################################
+# WRAPPING FUNCTIONS OF THE TWO MODULES #
+#########################################
 
-  args = parser.parse_args()
+def middle_one_away(args):
+  dumps_file = args.infile
+  output_pattern = args.o
+  k = int(args.k)
+
+  file_one_away_pairs = open(output_pattern + '_one_away_pairs.tsv', 'w')
+  file_coverages = open(output_pattern + '_coverages.tsv', 'w')
+
+  #Initialize dictionaries in which the key is a kmer_half (kmer_L or kmer_R respectively), and the value is a list of (other_kmer_half, index) pairs.
+  #kmer_L_to_index_family = defaultdict(list)
+  kmer_R_to_index_family = defaultdict(list)
+
+  #Get the locations for the two halves of the kmer.
+  k_L = k // 2
+  i_L_L = 0
+  i_L_R = k_L - 1
+  i_R_L = k_L
+  i_R_R = k-1
+
+  # Read each line of the input file in order to load the kmers and coverages and process the kmer halves.
+  current_kmer_L = ""
+  for i1, line in enumerate(dumps_file):
+    kmer, coverage1 = line.split()
+    coverage1 = int(coverage1)
+
+    #coverages.append(coverage)
+    new_kmer_L = kmer[i_L_L:i_L_R+1]
+    kmer_R = kmer[i_R_L+1:i_R_R+1]
+    if new_kmer_L == current_kmer_L:
+      if kmer_R in kmer_R_to_index_family:
+        for i2,coverage2 in kmer_R_to_index_family[kmer_R]:
+          if coverage2 < coverage1:
+            file_one_away_pairs.write(str(i2) + '\t' + str(i1) + '\n')
+            file_coverages.write(str(coverage2) + '\t' + str(coverage1) + '\n')
+          else:
+            file_one_away_pairs.write(str(i1) + '\t' + str(i2) + '\n')
+            file_coverages.write(str(coverage1) + '\t' + str(coverage2) + '\n')
+    else:
+      current_kmer_L = new_kmer_L
+      kmer_R_to_index_family = defaultdict(list)
+    kmer_R_to_index_family[kmer_R].append((i1,coverage1))
+
+  file_one_away_pairs.close()
+  file_coverages.close()
+
+def all_one_away(args):
   dumps_file = args.infile
   output_pattern = args.o
   k = int(args.k)
@@ -185,3 +224,25 @@ if __name__=='__main__':
           record_file2.write(str(cov2) + '\t' + str(cov1) + '\n')
 
   print('*_families_2.tsv and *_coverages_2.tsv files saved.')
+
+##############
+### SCRIPT ###
+##############
+
+if __name__=='__main__':
+  parser = argparse.ArgumentParser(description='Calculate unique kmer pairs from a Jellyfish or KMC dump file.')
+  parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='Alphabetically sorted Jellyfish or KMC dump file (stdin).')
+  parser.add_argument('-o', help='The pattern used to name the output (kmerpairs).', default='kmerpairs')
+  parser.add_argument('-k', help='The length of the kmer.', default=21)
+  parser.add_argument('-t', help='Number of processes to use.', default = 4)
+  parser.add_argument('--all', dest='all', action='store_const', const = True, default = False,
+                      help='Get all kmer pairs one SNP away from each other (default: just the middle one).')
+
+  args = parser.parse_args()
+
+  if args.all:
+    all_one_away(args)
+  else:
+    middle_one_away(args)
+
+  print('Done!')
