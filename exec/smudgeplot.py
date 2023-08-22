b9de3947f66e8a0d44d7c442f3b76a1d3e9c44ea
KamilSJaron <kamiljaron@gmail.com>
2023-08-22 23:41:58 +0200

    adding PloidyPlot to the pythonic interface

--- exec/smudgeplot.py
+++ exec/smudgeplot.py
@@ -20,10 +20,11 @@ class parser():
         argparser = argparse.ArgumentParser(
             # description='Inference of ploidy and heterozygosity structure using whole genome sequencing data',
             usage='''smudgeplot <task> [options] \n
-tasks: cutoff    Calculate meaningful values for lower/upper kmer histogram cutoff.
-       hetkmers  Calculate unique kmer pairs from a Jellyfish or KMC dump file.
-       plot      Generate 2d histogram; infere ploidy and plot a smudgeplot.
-       extract   Extract kmer pairs within specified coverage sum and minor covrage ratio ranges\n\n''')
+tasks: cutoff    Calculate meaningful values for lower kmer histogram cutoff.
+       hetmers   Calculate unique kmer pairs from a FastK k-mer database.
+       plot      Generate 2d histogram; infere ploidy and plot a smudgeplot.\n\n''')
+        # removing this for now;
+        #        extract   Extract kmer pairs within specified coverage sum and minor covrage ratio ranges
         argparser.add_argument('task', help='Task to execute; for task specific options execute smudgeplot <task> -h')
         argparser.add_argument('-v', '--version', action="store_true", default = False, help="print the version and exit")
         # print version is a special case
@@ -44,16 +45,18 @@ tasks: cutoff    Calculate meaningful values for lower/upper kmer histogram cuto
             sys.stderr.write('"' + self.task + '" is not a valid task name\n')
             exit(1)
 
-    def hetkmers(self):
+    def hetmers(self):
         '''
         Calculate unique kmer pairs from a Jellyfish or KMC dump file.
         '''
         argparser = argparse.ArgumentParser(prog = 'smudgeplot hetkmers',
-            description='Calculate unique kmer pairs from a Jellyfish or KMC dump file.')
-        argparser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='Alphabetically sorted Jellyfish or KMC dump file (stdin).')
+            description='Calculate unique kmer pairs from FastK k-mer database.')
+        argparser.add_argument('infile', nargs='?', help='Input FastK database (.ktab) file.')
+        argparser.add_argument('-L', help='Count threshold below which k-mers are considered erroneous', type=int)
+        argparser.add_argument('-t', help='Number of threads (default 4)', type=int, default=4)
         argparser.add_argument('-o', help='The pattern used to name the output (kmerpairs).', default='kmerpairs')
-        argparser.add_argument('--middle', dest='middle', action='store_const', const = True, default = False,
-                               help='Get all kmer pairs that are exactly the same but in the middle nt. When this flag is used, the input dump must be alphabetically sorted/ (default: different by a SNP at any position).')
+        argparser.add_argument('-tmp', help='Directory where all temporary files will be stored (default /tmp).', default='.')
+        argparser.add_argument('--verbose', action="store_true", default = False, help='verbose mode')
         self.arguments = argparser.parse_args(sys.argv[2:])
 
     def plot(self):
@@ -137,192 +140,6 @@ def cutoff(args):
         sys.stdout.write(str(U))
     sys.stdout.flush()
 
-############
-# hetkmers #
-############
-
-def get_one_away_pairs(kmer_index_family, k):
-    """kmer_index_family is a list of (kmer, index) pairs currently under consideration. k is the kmer length. get_one_away_pairs returns a list of pairs of indices where each pair of indices corresponds to a pair of kmers different in exactly one base."""
-
-    #This is the base case for the recursion. Return every pair of indices where the kmers corresponding to those indices differ at exactly one base.
-    if k == 1:
-        return [(i,j) for ((kmer1,i),(kmer2,j)) in combinations(kmer_index_family, 2) if kmer1 != kmer2]
-
-    #Initialize one_away_pairs, which will be returned by get_one_away_pairs.
-    one_away_pairs = []
-
-    #Initialize dictionaries in which the key is a kmer_half (kmer_L or kmer_R) and the value is a list of (other_kmer_half, index) pairs.
-    kmer_L_to_index_family = defaultdict(list)
-    kmer_R_to_index_family = defaultdict(list)
-
-    #Get the locations for the two halves of the kmer.
-    k_L = k // 2
-    k_R = k-k_L
-    i_L_L = 0
-    i_L_R = k_L - 1
-    i_R_L = k_L
-    i_R_R = k-1
-
-    #For each kmer and index calculate the corresponding left half and right half, then add the necessary (kmer_half, index) pair to the corresponding entries of the dictionary
-    for kmer, i in kmer_index_family:
-        kmer_L = kmer[i_L_L:i_L_R+1]
-        kmer_R = kmer[i_R_L:i_R_R+1]
-        kmer_L_to_index_family[kmer_L].append((kmer_R, i))
-        kmer_R_to_index_family[kmer_R].append((kmer_L, i))
-
-    #For each left half in which there are multiple kmers with that left half, find the list of pairs in which the right half differs by 1. (aka, if left half matches, recurse on right half).
-    for kmer_L_index_family in kmer_L_to_index_family.values(): #same in left half
-        if len(kmer_L_index_family) > 1:
-            one_away_pairs.extend(get_one_away_pairs(kmer_L_index_family, k_R)) #differ by 1 in right half
-
-    del kmer_L_to_index_family
-
-    #For each right half in which there are multiple kmers with that same right half, find the list of pairs in which the left half differs by 1. (aka, if right half matches, recurse on left half).
-    for kmer_R_index_family in kmer_R_to_index_family.values(): #same in right half
-        if len(kmer_R_index_family) > 1:
-            one_away_pairs.extend(get_one_away_pairs(kmer_R_index_family, k_L)) #differ by 1 in left half
-
-    del kmer_R_to_index_family
-    return(one_away_pairs)
-
-def middle_one_away(args):
-    sys.stderr.write('Extracting kmer pairs that differ in the middle nt\n')
-
-    # file_one_away_pairs = open(args.o + '_one_away_pairs.tsv', 'w')
-    file_coverages = open(args.o + '_coverages.tsv', 'w')
-    file_kmers = open(args.o + '_sequences.tsv', 'w')
-
-    duplicated = set()
-    filtered = set()
-
-    #Initialize a dictionary in which the key is the right kmer_half (not including the middle nucleotide), and the value is a list of (index, coverage) tuples corresponding to kmers that have that particular right kmer_half.
-    kmer_R_to_index_family = defaultdict(list)
-
-    # read the first line to get the length of the kmer
-    with open(args.infile.name) as dump_file:
-        kmer, coverage = dump_file.readline().split()
-        k = len(kmer)
-
-    #Get the locations for the two halves of the kmer.
-    k_middle = k // 2
-    i_L_L = 0
-    i_L_R = k_middle - 1
-    i_R_L = k_middle + 1
-    i_R_R = k - 1
-
-    sys.stderr.write('Saving ' + args.o + '_coverages.tsv and ' + args.o + '_sequences.tsv files.\n')
-    # Read each line of the input file in order to load the kmers and coverages and process the kmer halves.
-    current_kmer_L = ""
-    for i1, line in enumerate(args.infile):
-        kmer, coverage1 = line.split()
-        coverage1 = int(coverage1)
-
-        new_kmer_L = kmer[i_L_L:i_L_R+1]
-        kmer_R = kmer[i_R_L:i_R_R+1]
-        if new_kmer_L == current_kmer_L:
-            if kmer_R in kmer_R_to_index_family:
-                if kmer_R in duplicated:
-                    filtered.discard(kmer_R)
-                else:
-                    duplicated.add(kmer_R)
-                    filtered.add(kmer_R)
-        else:
-            for kmer_R in filtered:
-                (i1, coverage1), (i2, coverage2) = kmer_R_to_index_family[kmer_R]
-                if coverage2 < coverage1:
-                    file_coverages.write(str(coverage2) + '\t' + str(coverage1) + '\n')
-                else:
-                    file_coverages.write(str(coverage1) + '\t' + str(coverage2) + '\n')
-                file_kmers.write(current_kmer_L + 'N' + kmer_R + '\n')
-            duplicated = set()
-            filtered = set()
-            kmer_R_to_index_family = defaultdict(list)
-            current_kmer_L = new_kmer_L
-        kmer_R_to_index_family[kmer_R].append((i1,coverage1))
-
-    file_coverages.close()
-    file_kmers.close()
-
-def all_one_away(args):
-    #Initiate kmer and coverages lists.
-    kmers = []
-    coverages = []
-
-    # Read each line of the input file in order to
-    # load the kmers and coverages and process the kmer halves.
-    for i, line in enumerate(args.infile):
-        kmer, coverage = line.split()
-        coverage = int(coverage)
-        coverages.append(coverage)
-        kmers.append(kmer)
-
-    sys.stderr.write('Kmers and coverages loaded.\n')
-
-    k = len(kmer) # all the kmers in the dump file have the same length, so I can just calc the number of nts in the last one
-    # get_one_away_pairs is a recursive function that gatheres indices of all kmer 1 SNP from each other
-    one_away_pairs = get_one_away_pairs([(kmer,i) for i,kmer in enumerate(kmers)], k)
-
-    sys.stderr.write('Kmer pairs identified.\n')
-
-    repeated = {}
-    for (i1, i2) in one_away_pairs:
-        repeated[i1] = i1 in repeated
-        repeated[i2] = i2 in repeated
-
-    sys.stderr.write('Kmers in unique kmer pairs identified.\n')
-    coverage_pair_hist = defaultdict(int)
-
-    with open(args.o + '_sequences.tsv', 'w') as file_seqs, open(args.o + '_coverages.tsv', 'w') as file_coverages:
-        for (i1, i2) in one_away_pairs:
-            if not repeated[i1] and not repeated[i2]:
-                cov1 = coverages[i1]
-                cov2 = coverages[i2]
-                if cov1 < cov2:
-                    file_coverages.write(str(cov1) + '\t' + str(cov2) + '\n')
-                    file_seqs.write(kmers[i1] + '\t' + kmers[i2] + '\n')
-                    coverage_pair_hist[(cov1,cov2)] += 1
-                else:
-                    file_coverages.write(str(cov2) + '\t' + str(cov1) + '\n')
-                    file_seqs.write(kmers[i2] + '\t' + kmers[i1] + '\n')
-                    coverage_pair_hist[(cov2,cov1)] += 1
-
-    sys.stderr.write(args.o + '_families.tsv and ' + args.o + '_coverages.tsv files saved.\n')
-
-    with open(args.o + '_coverage_pair_hist.tsv', 'w') as file_coverage_hist:
-        for cov1, cov2 in coverage_pair_hist:
-            file_coverage_hist.write(str(cov1) + '\t' + str(cov2) + '\t' + str(coverage_pair_hist[(cov1, cov2)]) + '\n')
-
-    sys.stderr.write(args.o + '_coverage_pair_hist.tsv file saved.\n')
-
-def extract_kmer_pairs(args):
-    index2covs = defaultdict(list)
-
-    with open(args.coverageFile,"r") as f:
-        for i, row in enumerate(f):
-            row_list = row.rstrip("\n").split("\t")
-            countL = int(row_list[0])
-            countR = int(row_list[1])
-
-            cov_sum = countL + countR
-            cov_ratio = countL / (countL + countR)
-            if cov_sum <= args.countMax and cov_sum >= args.countMin and cov_ratio >= args.ratioMin and cov_ratio <= args.ratioMax:
-                # out_cov_h.write(str(countL)+"\t" + str(countR) + "\n")
-                #print(str(i1))
-                index2covs[i] = [countL, countR]
-
-    with open(args.seqFile,"r") as f:
-        extracted = 0
-        for i, row in enumerate(f):
-            if len(index2covs[i]) == 2:
-                extracted += 1;
-                kmer1, kmer2 = row.rstrip('\n').split('\t')
-                sys.stdout.write(">kmer_" + str(i) + "_1_cov_" + str(index2covs[i][0]) + "\n")
-                sys.stdout.write(kmer1 + '\n')
-                sys.stdout.write(">kmer_" + str(i) + "_2_cov_" + str(index2covs[i][1]) + "\n")
-                sys.stdout.write(kmer2 + '\n')
-        sys.stderr.write("Extracting " + str(extracted) + " of total kmer pairs: " + str(i + 1) + '\n')
-        sys.stderr.write(str(round(100 * extracted / (i + 1), 3)) + ' %\n')
-
 #####################
 # the script itself #
 #####################
@@ -339,12 +156,20 @@ def main():
     if _parser.task == "cutoff":
         cutoff(_parser.arguments)
 
-    if _parser.task == "hetkmers":
+    if _parser.task == "hetmers":
+        # PloidyPlot is expected ot be installed in the system as well as the R library supporting it
         args = _parser.arguments
-        if args.middle:
-            middle_one_away(args)
-        else :
-            all_one_away(args)
+        plot_args = " -o" + str(args.o)
+        plot_args += " -e" + str(args.L)
+        plot_args += " -T" + str(args.t)
+        if args.verbose:
+            plot_args += " -v"
+        if args.tmp != '.':
+            plot_args += " -P" + args.tmp
+        plot_args += " " + args.infile
+
+        sys.stderr.write("Calling: PloidyPlot " + plot_args + "\n")
+        system("PloidyPlot " + plot_args)
 
     if _parser.task == "plot":
         # the plotting script is expected ot be installed in the system as well as the R library supporting it
