2687980409b8f8355326c7cdaf73f87c7c6af7de
KamilSJaron <kamiljaron@gmail.com>
2024-03-21 11:34:43 +0100

    adding plotting options that include filtering low coverage kmer pairs, no inference at all and specification of the y axis

--- R/plot_histograms.R
+++ R/plot_histograms.R
@@ -30,33 +30,39 @@ plot_histograms <- function(.cov_tab, .smudge_summary,
         mtext(bquote(italic(.(.fig_title))), side=3, adj=0, line=-3, cex = .cex + 0.2)
     }
 
-    if ( .smudge_summary$genome_ploidy < 9){
-        ploidytext <- switch(.smudge_summary$genome_ploidy - 1,
-                               p2 = 'diploid',
-                               p3 = 'triploid',
-                               p4 = 'tetraploid',
-                               p5 = 'pentaploid',
-                               p6 = 'hexaploid',
-                               p7 = 'heptaploid',
-                               p8 = 'octoploid')
-    } else {
-        ploidytext <- paste0(.smudge_summary$genome_ploidy, '-ploid')
-    }
+    if (length(.smudge_summary$genome_ploidy) > 0){
+        if ( .smudge_summary$genome_ploidy < 9){
+            ploidytext <- switch(.smudge_summary$genome_ploidy - 1,
+                                p2 = 'diploid',
+                                p3 = 'triploid',
+                                p4 = 'tetraploid',
+                                p5 = 'pentaploid',
+                                p6 = 'hexaploid',
+                                p7 = 'heptaploid',
+                                p8 = 'octoploid')
+        } else {
+            ploidytext <- paste0(.smudge_summary$genome_ploidy, '-ploid')
+        }
 
-    if(!(is.na(.smudge_summary$genome_ploidy))){
-        mtext(paste('proposed', ploidytext), side=3, adj=0.05, line=-5, cex = .cex - 0.2)
-    }
+        if(!(is.na(.smudge_summary$genome_ploidy))){
+            mtext(paste('proposed', ploidytext), side=3, adj=0.05, line=-5, cex = .cex - 0.2)
+        }
+    } 
+
 
     # total pair coverage HISTOGRAM - right
     coverage_histogram(.cov_tab, bins = .bins, xlim = .ylim, # this is because horizonal is TRUE 
                        column = 'total_pair_cov', horiz = TRUE, .col)
-    legend('bottomright', bty = 'n', paste('1n = ', round(.smudge_summary$n)), cex = .cex - 0.1)
 
-    .peak_sizes <- data.frame(peak = reduce_structure_representation(.smudge_summary$peak_sizes),
-                              size = .smudge_summary$peak_sizes[,3])
+    if (.smudge_summary$n > 0){ #0 means it was not infered
+        legend('bottomright', bty = 'n', paste('1n = ', round(.smudge_summary$n)), cex = .cex - 0.1)
 
-    if(! any(is.na(.peak_sizes))){
-        legend('topleft', bty = 'n', .peak_sizes[,1], cex = .cex - 0.2)
-        legend('topright', bty = 'n', legend = round(.peak_sizes[,2], 2), cex = .cex - 0.2)
-    }
+        .peak_sizes <- data.frame(peak = reduce_structure_representation(.smudge_summary$peak_sizes),
+                                size = .smudge_summary$peak_sizes[,3])
+
+        if(! any(is.na(.peak_sizes))){
+            legend('topleft', bty = 'n', .peak_sizes[,1], cex = .cex - 0.2)
+            legend('topright', bty = 'n', legend = round(.peak_sizes[,2], 2), cex = .cex - 0.2)
+        }
+    }                       
 }
